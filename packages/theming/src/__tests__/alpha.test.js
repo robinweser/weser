941a40173577d5a2268668c96d05c531ed90d7e3
Robin Weser <contact@weser.io>
2025-09-21 16:15:15 +0200

    fix tests

--- packages/theming/src/__tests__/alpha.test.js
+++ packages/theming/src/__tests__/alpha.test.js
@@ -3,5 +3,8 @@ import test from 'ava'
 import alpha from '../../dist/alpha.js'
 
 test('alterating alpha should work', (t) => {
-  t.is(alpha('var(--red)', 0.2), 'hsl(from var(--red) h s l / 0.2)')
+  t.is(
+    alpha('var(--red)', 0.2),
+    'hsl(from var(--red) h s l / max(0, min(1, 0.2)))'
+  )
 })
