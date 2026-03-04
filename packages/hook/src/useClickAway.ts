0a376416628c78dcc03e9ba374895b2822588428
Robin Weser <contact@weser.io>
2026-03-04 19:32:41 +0100

    add ignoreRefs to useClickAway

--- packages/hook/src/useClickAway.ts
+++ packages/hook/src/useClickAway.ts
@@ -1,27 +1,29 @@
-import { RefObject, useEffect } from 'react'
+import { useEffect, type RefObject } from 'react'
 
 export default function useClickAway(
   ref: RefObject<HTMLElement | null>,
   callback: () => void,
-  active: boolean = true
+  active = true,
+  ignoreRefs?: RefObject<HTMLElement | null>[]
 ) {
   useEffect(() => {
-    const onClickAway = (e: Event) => {
+    const onClickAway = (e: MouseEvent | TouchEvent) => {
       if (active && ref.current) {
-        const isClickOnInner = ref.current.contains(e.target as Node)
+        const target = e.target as Node
+        const isClickOnInner = ref.current.contains(target)
+        const isClickOnIgnored =
+          ignoreRefs?.some((r) => r.current?.contains(target)) ?? false
 
-        if (!isClickOnInner) {
+        if (!isClickOnInner && !isClickOnIgnored) {
           setTimeout(callback, 0)
         }
       }
     }
-
     document.addEventListener('mousedown', onClickAway)
     document.addEventListener('touchstart', onClickAway)
-
     return () => {
       document.removeEventListener('mousedown', onClickAway)
       document.removeEventListener('touchstart', onClickAway)
     }
-  }, [ref, active, callback])
+  }, [ref, active, callback, ignoreRefs])
 }
